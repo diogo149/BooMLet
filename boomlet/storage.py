9f82d3280e6f99f2bc405a601f9136e2f99bb5b3
Diogo Moitinho de Almeida <diogo149@gmail.com>
2013-12-15 17:05:10 -0800

    porting and cleaning up storage functions

---
+++ boomlet/storage.py
@@ -0,0 +1,83 @@
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+import dill
+import joblib
+import zlib
+import os
+
+
+def mkdir(filename):
+    """ try to make directory
+    """
+    try:
+        os.makedirs(os.path.dirname(filename))
+    except OSError:
+        pass
+
+
+def exists(filename):
+    return os.path.exists(filename)
+
+
+def writes(filename, s):
+    with open(filename, 'w') as outfile:
+        outfile.write(s)
+
+
+def reads(filename):
+    with open(filename) as infile:
+        return infile.read()
+
+
+def compress(s, level=9):
+    return zlib.compress(s, level)
+
+
+def decompress(s):
+    return zlib.decompress(s)
+
+
+def pickle_load(filename):
+    with open(filename) as infile:
+        return pickle.load(infile)
+
+
+def pickle_dump(filename, obj):
+    with open(filename, 'w') as outfile:
+        pickle.dump(obj, outfile, pickle.HIGHEST_PROTOCOL)
+
+
+def pickle_loads(s):
+    return pickle.loads(s)
+
+
+def pickle_dumps(obj):
+    return pickle.dumps(obj, pickle.HIGHEST_PROTOCOL)
+
+
+def joblib_load(filename, mmap_mode='r'):
+    return joblib.load(filename, mmap_mode)
+
+
+def joblib_dump(filename, obj, compress=0):
+    return joblib.dump(obj, filename, compress)
+
+
+def dill_load(filename):
+    with open(filename) as infile:
+        return dill.load(infile)
+
+
+def dill_dump(filename, obj):
+    with open(filename, 'w') as outfile:
+        dill.dump(obj, outfile)
+
+
+def dill_loads(s):
+    return dill.loads(s)
+
+
+def dill_dumps(obj):
+    return dill.dumps(obj)
