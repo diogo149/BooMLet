bc72166c57692f752a36e392d274e4f0dae8eb7a
Diogo Moitinho de Almeida <diogo149@gmail.com>
2013-12-19 19:44:01 -0800

    orthogonal polynomial transform

--- boomlet/transform/__init__.py
+++ boomlet/transform/__init__.py
@@ -1,6 +1,9 @@
 from sparse_filtering import SparseFiltering
 from gap_statistic import FitClusterer
+from orthogonal_polynomial import OrthogonalPolynomialTransformer
 
 __all__ = [
     'SparseFiltering',
+    'FitClusterer',
+    'OrthogonalPolynomialTransformer',
     ]
