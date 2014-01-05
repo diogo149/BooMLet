76014798df6f8ee3b6be97675e1bc3fa5641bfcb
Diogo Moitinho de Almeida <diogo149@gmail.com>
2014-01-05 09:43:09 -0800

    adding random seeding context manager

--- boomlet/contextmanagers.py
+++ boomlet/contextmanagers.py
@@ -1,9 +1,33 @@
 from time import time
 from contextlib import contextmanager
+import random
 
+import numpy as np
 
 @contextmanager
 def timer(name=""):
     start_time = time()
     yield
     print("{} took {}s".format(name, time() - start_time))
+
+
+@contextmanager
+def seed_random(seed=None):
+    """
+    seeds RNG for both random and numpy.random
+    """
+    if seed is None:
+        yield
+    elif isinstance(seed, int):
+        # save state
+        random_state = random.getstate()
+        np_random_state = np.random.get_state()
+        # set state
+        random.seed(seed)
+        np.random.seed(seed)
+        yield
+        # reset state
+        random.setstate(random_state)
+        np.random.set_state(np_random_state)
+    else:
+        raise TypeError("Improper random seed type")
